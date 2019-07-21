76d787107026ab74672308763d5dc95e68277d95
Julian Astrada <>
2019-07-21 14:35:46 -0300

    Initial commit

---
+++ RedditClient/RedditClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedditClient
+//
+//  Created by Julian Astrada on 21/07/2019.
+//  Copyright © 2019 Julian Astrada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
